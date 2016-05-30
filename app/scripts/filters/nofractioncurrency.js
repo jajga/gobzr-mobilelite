f5f8812298c6998f07a306d2f30979c0a29dd17c
User <User>
2016-05-30 15:10:12 +0530

    no currency

---
+++ app/scripts/filters/nofractioncurrency.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name gobzrliteApp.filter:noFractionCurrency
+ * @function
+ * @description
+ * # noFractionCurrency
+ * Filter in the gobzrliteApp.
+ */
+angular.module('gobzrliteApp')
+  .filter('noFractionCurrency',
+  [ '$filter', '$locale',
+  function(filter, locale) {
+    var currencyFilter = filter('currency');
+    var formats = locale.NUMBER_FORMATS;
+    return function(amount, currencySymbol) {
+      var value = currencyFilter(amount, currencySymbol);
+      var sep = value.indexOf(formats.DECIMAL_SEP);
+      if(amount >= 0) { 
+        return value.substring(0, sep);
+      }
+      return value.substring(0, sep) + ')';
+    };
+  } ]);
