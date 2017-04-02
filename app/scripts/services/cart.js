57c6370774594b62655ed9a2097396b1c2a21224
System Administrator <root@Narendras-MacBook-Pro.local>
2017-04-02 14:18:19 +0530

    new changed con

--- app/scripts/services/cart.js
+++ app/scripts/services/cart.js
@@ -11,6 +11,7 @@ angular.module('gobzrliteApp')
   .service('cartService', function (global,$http,$cookies) {
   	console.log("hello");
     // AngularJS will instantiate a singleton by calling "new" on this function
+    console.log('hello4');
     this.getCart=function(formData){
     //var finalorderId= 0 ;
     var promise = $http.post(global.API_BUSS+'cart/getcart?orderId=0',formData);
