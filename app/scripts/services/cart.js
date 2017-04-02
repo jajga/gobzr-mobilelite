b6772d9c59e74f433a58ce00b35cab14816a5990
System Administrator <root@Narendras-MacBook-Pro.local>
2017-04-03 01:00:40 +0530

    con 8

--- app/scripts/services/cart.js
+++ app/scripts/services/cart.js
@@ -17,6 +17,7 @@ angular.module('gobzrliteApp')
     console.log('hello6');
     console.log('hello5');
     console.log('hello7');
+    console.log('hello8');
     this.getCart=function(formData){
     //var finalorderId= 0 ;
     var promise = $http.post(global.API_BUSS+'cart/getcart?orderId=0',formData);
