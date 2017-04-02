73011f33f358c940aa61bd48ca01c8dfc4d49456
System Administrator <root@Narendras-MacBook-Pro.local>
2017-04-03 00:12:10 +0530

    console 5

--- app/scripts/services/cart.js
+++ app/scripts/services/cart.js
@@ -14,6 +14,7 @@ angular.module('gobzrliteApp')
   	console.log("hello3");
     // AngularJS will instantiate a singleton by calling "new" on this function
     console.log('hello4');
+    console.log('hello5');
     this.getCart=function(formData){
     //var finalorderId= 0 ;
     var promise = $http.post(global.API_BUSS+'cart/getcart?orderId=0',formData);
